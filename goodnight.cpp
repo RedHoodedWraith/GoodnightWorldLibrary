8dd00558b4853a8eef6fe8c7e5657e4f5197a3fc
Rowan Rathod <stem@rowanrathod.com>
2020-11-17 17:13:58 +1300

    Initial commit

---
+++ goodnight.cpp
@@ -0,0 +1,9 @@
+//
+// Created by rowan on 11/11/20.
+//
+
+#include "goodnight.h"
+
+void goodnight::sayGoodnight(std::string p) {
+    std::cout << GOODNIGHT_MSG << p << "." << std::endl;
+}
---
+++ goodnight.h
@@ -0,0 +1,19 @@
+//
+// Created by rowan on 11/11/20.
+//
+
+#ifndef GOODNIGHTWORLDLIBRARY_GOODNIGHT_H
+#define GOODNIGHTWORLDLIBRARY_GOODNIGHT_H
+
+#include <iostream>
+#include <string>
+
+#define GOODNIGHT_MSG "Goodnight "
+
+class goodnight {
+public:
+    void sayGoodnight(std::string p);
+};
+
+
+#endif //GOODNIGHTWORLDLIBRARY_GOODNIGHT_H
